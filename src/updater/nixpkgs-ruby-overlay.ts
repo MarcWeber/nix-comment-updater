22af800e4e66abe4b258b2f6e9dc572caaf1634f
Marc Weber <marco-oweber@gmx.de>
2023-01-25 12:35:32 +0100

    updates ruby

--- src/updater/nixpkgs-ruby-overlay.ts
+++ src/updater/nixpkgs-ruby-overlay.ts
@@ -9,12 +9,16 @@ export type NixpkgsRubyOverlayJson = {
 
     "nixpkgs-ruby-overlay": string,
 
+    ruby_version?: string
+
     // the packages you need eg ['
     deps: string[],
 
     // sometimes some dependencies are missing others. you can use deps_patches to add them
     // { :deps => [ ['unicode'], [ 'xapian-full'], ['sup'], ['gpgme'], ['rake'], ['syck'], ['gettext'], ['ncursesw'], ['curses'], ['iconv'] ], :deps_patches => {'sup' => ['xapian-ruby'], 'xapian-ruby' => ['rake']} }
     deps_patches: Record< string, string[]>
+
+    "add-dev-packages"?: true
 }
 
 let ruby = undefined;
@@ -24,11 +28,19 @@ export const updater: UpdateBlockFunction<NixpkgsRubyOverlayJson> = async (o) =>
 
     const ruby = await nixpkgs_executable("ruby")
 
+    let deps = [...j.deps]
+
+    if (j["add-dev-packages"])
+        // some of the other tools eventually are more annoying then helpful ?
+        deps = [...deps, "solargraph", "rubocop", "reek"]
+
     if ('nixpkgs-ruby-overlay' in j) {
-        const v = {name: j["nixpkgs-ruby-overlay"], deps: j.deps, deps_patches: j.deps_patches ?? {}}
+
+        const ruby_version = 'ruby_version' in j ? ['--ruby-version', j.ruby_version] : []
+        const v = {name: j["nixpkgs-ruby-overlay"], deps: deps, deps_patches: j.deps_patches ?? {}}
         fs.writeFileSync("/tmp/ruby-deps-to-nix-json-deps", JSON.stringify(v), 'utf8')
         return action(`nixpkgs-ruby-overlay ${v} ${o.region.filename}`,
-            () => spawn([ruby, 'ruby-deps-to-nix.rb', '--cache-file',  '/tmp/ruby2nix', '--json-deps-file', '/tmp/ruby-deps-to-nix-json-deps'], {
+            () => spawn([ruby, 'ruby-deps-to-nix.rb'] + ruby_version + [ '--cache-file',  '/tmp/ruby2nix', '--json-deps-file', '/tmp/ruby-deps-to-nix-json-deps'], {
                 'cwd': NIX_PATH['nixpkgs-ruby-overlay'] ?? '/etc/nixos/nixpkgs-ruby-overlay',
             }).promise.then((x) => x.out))
     }
--- src/updater/ruby-marc-key.ts
+++ src/updater/ruby-marc-key.ts
@@ -5,14 +5,16 @@ import { action } from "../utils"
 
 export type RubyMarcJson = {
     ruby_marc_key: string
+    ruby_version?: string
 }
 
 export const updater: UpdateBlockFunction<RubyMarcJson> = async (o) => {
     const j = o.json
     if ('ruby_marc_key' in j) {
         const v = j.ruby_marc_key
+        const ruby_version = 'ruby_version' in j ? `--ruby-version ${j.ruby_version}` : ""
         return action(`nixpkgs-ruby-overlay ${v} ${o.region.filename}`,
-            () => spawn(['nix-shell', '-p', 'ruby', '--run', `ruby get.rb --as-env ${v}`], {
+            () => spawn(['nix-shell', '-p', 'ruby', '--run', `ruby get.rb ${ruby_version} --as-env ${v}`], {
                 'cwd': '/etc/nixos/nixpkgs-ruby-overlay',
             }).promise.then((x) => x.out))
     }
